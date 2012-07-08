e6234373e63ea35efec3f18279e702e2a8426bb1
yukaihg <yukaihg@gmail.com>
2012-07-08 15:21:22 -0700

    fixed bugs

--- controller/queryES.js
+++ controller/queryES.js
@@ -89,7 +89,7 @@ QueryES.prototype.getAllQuestionByUserID = function(userID, appType, callback){
 QueryES.prototype.searchAll = function(search, appType, callback){
 
 	if(!search){
-		console.log("empty");
+		callback(undefined);
 		return;
 	}
 
@@ -111,7 +111,7 @@ QueryES.prototype.searchAll = function(search, appType, callback){
 	switchIndex(appType);
 	switchMapping(0);
 
-	index.search(data, function(err, data){
+	mapping.search(data, function(err, data){
 		if(data && data.hits.total !== 0) {
 			callback(data.hits);
 		} else { 

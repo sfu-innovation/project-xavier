003613cd9892c88619f787a87d1b23da98fceaba
Mark Ni <xixian.ni@gmail.com>
2012-07-09 17:54:17 -0700

    [#32413987] fixed a problem that won't create question.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -148,7 +148,7 @@ $(document).ready(function () {
 	$('#createQuestion').click(function (event) {
 		var title = $('#new_question_title').val();
 		var body = $('#new_question_body').val();
-		if (user_id && title && body) {
+		if (title && body) {
 			rqra.createQuestion(title, body, function (data) {
 				if (data) {
 					if (data.errorcode === 0) {

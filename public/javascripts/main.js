c1bd9d56543f062734a8453146c4cd4bcc50831d
Mark Ni <xixian.ni@gmail.com>
2012-07-09 17:49:28 -0700

    [#32413977] mapping for getCommentByUserID in front-end

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -274,7 +274,54 @@ $(document).ready(function () {
 		else {
 			$('#error').text('CANNOT HAVE EMPTY FILED');
 		}
-	})
+	});
+
+
+
+	$('#getCommentsByUserId').click(function (event) {
+		var user_id = $("#comment_user_id").val();
+		if (user_id) {
+			rqra.getCommentsByUserId(user_id, function (data) {
+				if (data) {
+					console.log(data);
+
+
+					$('#user_comments').empty();
+					if (data.errorcode === 0 && data.comments.length > 0) {
+
+						$.each(data.comments, function (index, item) {
+							console.log(item);
+							var content = '<li>'
+								+ '<p>_id: ' + item._id + '</p>'
+								+ '<p>body: ' + item._source.body + '</p>'
+								+ '<p>downvote: ' + item._source.downvote + '</p>'
+								+ '<p>isAnswered: ' + item._source.isAnswered + '</p>'
+								+ '<p>objectType: ' + item._source.objectType + '</p>'
+								+ '<p>target_uuid: ' + item._source.target_uuid + '</p>'
+								+ '<p>timestamp: ' + item._source.timestamp + '</p>'
+								+ '<p>title: ' + item._source.title + '</p>'
+								+ '<p>upvote: ' + item._source.upvote + '</p>'
+								+ '<p>user: ' + item._source.user + '</p>'
+								+ '</li>';
+							$('#user_comments').append(content);
+						});
+					}
+					else {
+						$('#error').text(data.message);
+					}
+
+				}
+				else {
+					$('#error').text('CANNOT CONNECT TO DATABASE');
+				}
+			});
+		}
+		else {
+			$('#error').text('CANNOT BE EMPTY FILED');
+		}
+
+
+	});
 
 
 });

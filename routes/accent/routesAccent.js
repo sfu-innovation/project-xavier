c673c1ef692e0d49915a9580e48d59e70c2b7a0c
Jihoon <jhc20@sfu.ca>
2012-07-12 14:29:51 -0700

    modified to use TagAction instead of the functions from tag model.

--- routes/accent/routesAccent.js
+++ routes/accent/routesAccent.js
@@ -1,5 +1,6 @@
 var routesPresenter = require("./../rqra/routesPresenter.js");
 
+var TagAction = require("./../../controller/TagAction.js");
 var Tag = require("./../../models/tag");
 
 exports.question = function(request, response) {
@@ -66,8 +67,20 @@ exports.unfollowQuestion = function(request, response) {
 exports.tag = function(request,response){
 	//TODO: replace this with seesion user
 
-	request.body.tag.user_uid = 'fakeid';
+	request.body.tag.user = 'fakeid';
 	if(request.method === 'POST'){
+		TagAction.addTag(request.body.tag, function(error, result){
+			if(result){
+				response.writeHead(200, { 'Content-Type': 'application/json' });
+				response.end(JSON.stringify({ errorcode: 0, resource: result }));
+			}
+			else{
+				response.writeHead(200, { 'Content-Type': 'application/json' });
+				response.end(JSON.stringify({ errorcode: 1, message: error }));
+			}
+		});
+
+		/*
 		Tag.createTag(request.body.tag, function(error, result){
 			if(result){
 				response.writeHead(200, { 'Content-Type': 'application/json' });
@@ -78,8 +91,21 @@ exports.tag = function(request,response){
 				response.end(JSON.stringify({ errorcode: 1, message: error }));
 			}
 		});
+		*/
 	}
 	else if (request.method === 'GET'){
+		TagAction.viewTags(request.body.tag, function(error, result){
+			if(result){
+				response.writeHead(200, { 'Content-Type': 'application/json' });
+				response.end(JSON.stringify({ errorcode: 0, resource: result }));
+			}
+			else{
+				response.writeHead(200, { 'Content-Type': 'application/json' });
+				response.end(JSON.stringify({ errorcode: 1, message: error }));
+			}
+		});		
+
+		/*
 		Tag.selectTag(request.body.tag, function(error,result){
 			if(result){
 				response.writeHead(200, { 'Content-Type': 'application/json' });
@@ -91,9 +117,22 @@ exports.tag = function(request,response){
 			}
 
 		})
+		*/
 
 	}
 	else if (request.method === 'PUT'){
+		TagAction.updateTag(request.body.tag, function(error, result){
+			if(result){
+				response.writeHead(200, { 'Content-Type': 'application/json' });
+				response.end(JSON.stringify({ errorcode: 0, resource: result }));
+			}
+			else{
+				response.writeHead(200, { 'Content-Type': 'application/json' });
+				response.end(JSON.stringify({ errorcode: 1, message: error }));
+			}
+		});	
+
+		/*				
 		Tag.updateTag(request.body.tag, function(error,result){
 			if(result){
 				response.writeHead(200, { 'Content-Type': 'application/json' });
@@ -105,9 +144,11 @@ exports.tag = function(request,response){
 			}
 
 		})
+		*/
 	}
 	else if (request.method === 'DELETE'){
 		//TODO: no method found yet
+		// coming soon...
 
 	}
 

e196f9aac7fdfad317e901589860fcb0dffb0bbc
Mike <mike.klemarewski@gmail.com>
2012-07-13 15:38:28 -0700

    Refactoring tests, added getProfile test

--- tests/common/UserRestUnitTest.js
+++ tests/common/UserRestUnitTest.js
@@ -15,69 +15,93 @@ module.exports = {
 		// get the details of the question created
 		
 		setUp: function(callback) {
-			queries.createDB(config.mysqlDatabase["db-name"], function(){
-				console.log("created database");
-				var self = this;
-				this.server = express.createServer();
-				this.server.use(server)
-				this.server.listen(function() {
-					self.port = this.address().port;
-					callback();
+		
+
+			var that = this;
+
+			queries.dropDB(config.mysqlDatabase["db-name"], function(){
+				queries.createDB(config.mysqlDatabase["db-name"], function(){
+					
+					var newUser = {
+						"firstName":"Mike",
+						"lastName":"Klemarewski",
+						"type":1,
+						"userID":"mak10",
+						"email":"mak10@sfu.ca"
+					}
+
+					User.createUser(newUser, function(error, user){
+						if(user){
+							that.user = user;
+							that.server = express.createServer();
+							that.server.use(server);
+							that.server.listen(function() {
+								that.port = this.address().port;
+								callback();
+							});
+						}
+						else{
+							callback();
+						}
+					});
+
 				});
 			});
 		},
 		tearDown: function(callback){
-			queries.dropDB(config.mysqlDatabase["db-name"], function(){
-				this.server.close();
-				callback();
-			});
+			this.server.close();
+			callback();
 		},
 		
 		getUser: function(test){
+
+			options = {
+				host:config.presenterServer.host,
+				method:"GET",
+				path:"/api/user/" + this.user.uuid,
+				port:this.port,
+				headers: {
+					"content-type": "application/json"
+				}
+			};
 		
-			var newUser = {
-				"firstName":"Mike",
-				"lastName":"Klemarewski",
-				"type":1,
-				"userID":"mak10",
-				"email":"mak10@sfu.ca"
-			}
-
-			User.createUser(newUser, function(error, user){
-				console.log("created user");
-				if(user){
-					var user_id = user.uuid;
-					var options = {
-						host:config.presenterServer.host,
-						port:this.port,
-						method:"GET",
-						path:"/api/user/" + user_id,
-						headers: {
-							"content-type": "application/json"
-						}
-					}
-				
-					var request = http.get(options, function(response){
-						var body = "";
-						response.on('data', function (chunk) {
-							body += chunk;
-						}).on('end', function() {
-							body = JSON.parse(body);
-							test.ok(body.errorcode === 0 &&
-								body.user.firstName === "Mike" && 
-								body.user.userID === "mak10");
-							test.done();
-						});
-					});
-				}
-				else{
-					test.ok(false);
+			var request = http.get(options, function(response){
+				var body = "";
+				response.on('data', function (chunk) {
+					body += chunk;
+				}).on('end', function() {
+					body = JSON.parse(body);
+					test.ok(body.errorcode === 0 &&
+						body.user.firstName === "Mike" && 
+						body.user.userID === "mak10");
 					test.done();
+				});
+			});
+		},
+		getUserProfile: function(test){
+			var that = this;
+			options = {
+				host:config.presenterServer.host,
+				method:"GET",
+				path:"/api/user/" + this.user.uuid + "/profile",
+				port:this.port,
+				headers: {
+					"content-type": "application/json"
 				}
-			})
+			};
 
-			
+			var request = http.get(options, function(response){
+				var body = "";
+				response.on('data', function (chunk) {
+					body += chunk;
+				}).on('end', function() {
+					body = JSON.parse(body);
+					test.ok(body.errorcode === 0 &&
+						body.profile.user === that.user.uuid);
+					test.done();
+				});
+			});
 		}
-	},
+	}
 
 }

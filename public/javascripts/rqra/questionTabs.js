40063b8897d1e4affd240b517ffb9e84f04f3072
jrf2 <jrf2@sfu.ca>
2012-07-15 13:50:41 -0700

    added dummy code for paging, simply appending the same data when clicking the get more button. Will be ready to hook up paging when Yukai finishes it in the REST interface.

--- public/javascripts/rqra/questionTabs.js
+++ public/javascripts/rqra/questionTabs.js
@@ -1,13 +1,45 @@
-function displayAsked() {
+// keeps track of the current page
+var display = displayAll;
+var lastPage = 0;
+
+// resets the page tracking
+function getFirstAsked() {
+	lastPage = 0;
+	displayAsked(lastPage);
+	display = displayAsked;
+}
+
+function getFirstNew() {
+	lastPage = 0;
+	displayNew(lastPage);
+	display = displayNew;
+}
+
+function getFirstUnanswered() {
+	lastPage = 0;
+	displayUnanswered(lastPage);
+	display = displayUnanswered;
+}
+
+function getFirstAll() {
+	lastPage = 0;
+	displayAll(lastPage);
+	display = displayAll;
+}
+
+// appends data to the list
+function displayAsked(page) {
 
 }
 
-function displayNew() {
+function displayNew(page) {
 	var rqra = new coreApi.Presenter();
 	var questionList = document.getElementById("questionList");
 	rqra.getNewQuestions(function (data) {
 		if (data && data.errorcode === 0 && data.questions.length > 0) {
-			questionList.innerHTML = "";
+			if (page === 0) {
+				questionList.innerHTML = "";
+			}
 			$.each(data.questions, function (index, item) {
 				questionList.innerHTML += "<div class='question'>"
 					+ "<h2>" + item._source.title + "</h2>"
@@ -22,12 +54,14 @@ function displayNew() {
 	});
 }
 
-function displayUnanswered() {
+function displayUnanswered(page) {
 	var rqra = new coreApi.Presenter();
 	var questionList = document.getElementById("questionList");
 	rqra.getUnansweredQuestions(function (data) {
 		if (data && data.errorcode === 0 && data.questions.length > 0) {
-			questionList.innerHTML = "";
+			if (page === 0) {
+				questionList.innerHTML = "";
+			}
 			$.each(data.questions, function (index, item) {
 				questionList.innerHTML += "<div class='question'>"
 					+ "<h2>" + item._source.title + "</h2>"
@@ -42,12 +76,14 @@ function displayUnanswered() {
 	});
 }
 
-function displayAll() {
+function displayAll(page) {
 	var rqra = new coreApi.Presenter();
 	var questionList = document.getElementById("questionList");
 	rqra.getAllQuestions(function (data) {
 		if (data && data.errorcode === 0 && data.questions.length > 0) {
-			questionList.innerHTML = "";
+			if (page === 0) {
+				questionList.innerHTML = "";
+			}
 			$.each(data.questions, function (index, item) {
 				questionList.innerHTML += "<div class='question'>"
 					+ "<h2>" + item._source.title + "</h2>"
@@ -62,6 +98,11 @@ function displayAll() {
 	});
 }
 
+// gets more of the current page
+function displayMore() {
+	lastPage++;
+	display(lastPage);
+}
+
 // displays asked questions on page load
-displayAll();
-//loadAllQuestions(rqra);
+displayAll(0);

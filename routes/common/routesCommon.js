e4c6fa4307926d2b8f1eaf32ebf5978c6f1bfb23
yukaihg <yukaihg@gmail.com>
2012-07-20 13:44:41 -0700

    removed nlp stuff, omg more responsive

--- routes/common/routesCommon.js
+++ routes/common/routesCommon.js
@@ -974,25 +974,26 @@ exports.searchQuestionsRoute = function(appType, request, response){
 	var queryData = request.body;
 
 	if (request.method === "POST") {
+		QueryES.searchQuestionsRoute(appType, request.params.page, queryData, function(err, result){
+			if (!err) {
+				response.writeHead(200, { 'Content-Type': 'application/json' });
+				if(result){
+					response.end(JSON.stringify({ errorcode: 0, questions: result }));
+				}
+				else{
+					response.end(JSON.stringify({ errorcode: 0, questions: "No questions found" }));
+				}
+			} else {
+				response.writeHead(500, { 'Content-Type': 'application/json' });
+				response.end(JSON.stringify({ errorcode: 1, message: err }));
+			}
+		});
+		/*
 		nlp(queryData.searchQuery, function(query){
 			console.log('query after nlp parsing is: ' + query);
 
-			queryData.searchQuery = query;
+		 	//queryData.searchQuery = query;
 
-			QueryES.searchQuestionsRoute(appType, request.params.page, queryData, function(err, result){
-				if (!err) {
-					response.writeHead(200, { 'Content-Type': 'application/json' });
-					if(result){
-						response.end(JSON.stringify({ errorcode: 0, questions: result }));
-					}
-					else{
-						response.end(JSON.stringify({ errorcode: 0, questions: "No questions found" }));
-					}
-				} else {
-					response.writeHead(500, { 'Content-Type': 'application/json' });
-					response.end(JSON.stringify({ errorcode: 1, message: err }));
-				}
-			});
-		});
+		 });*/
 	}
 }

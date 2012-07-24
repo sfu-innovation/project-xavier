548dab0840cb5ed012f4c5967153e235e64dfa8d
Mark Ni <xixian.ni@gmail.com>
2012-07-24 03:26:37 -0700

    hedy is cleaning the courses everytime the page load .......has to comment out as the site breaks after changes pages.

--- app-engage.js
+++ app-engage.js
@@ -22,11 +22,11 @@ app.configure(function() {
 
 app.configure('development', function(){
 	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-	app.use(function(req, res, next) {
-		if (req.session)
-			req.session.courses = [ ];
-		next();
-	})
+//	app.use(function(req, res, next) {
+//		if (req.session)
+//			req.session.courses = [ ];
+//		next();
+//	})
 });
 
 app.configure('production', function(){

fda1d699ef2a3eeeeb803e2bb8494b8ae8a8c7e0
jrf2 <jrf2@sfu.ca>
2012-07-26 09:37:05 -0700

    displays 7 questions per page

--- controller/queryES.js
+++ controller/queryES.js
@@ -9,7 +9,7 @@ var es = require('com.izaakschroeder.elasticsearch'),
 	async = require('async'),
 	user = require('../models/user.js'),
 	userProfile = require('../models/userProfile.js'),
-	sizeOfResult = 3;
+	sizeOfResult = 7;
 
 var QueryES = function() {
 }

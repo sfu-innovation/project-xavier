15eed985fa12c8ae1a3f6003cbe2bcd3aa65bf1e
Mike Klemarewski <mike.klemarewski@gmail.com>
2012-07-26 15:52:08 -0700

    Added ffmpeg encoding

--- routes/accent/routesAccent.js
+++ routes/accent/routesAccent.js
@@ -8,6 +8,8 @@ var Tag          = require(__dirname + "/../../models/tag");
 var queryES      = require(__dirname + '/../../controller/queryES.js');
 var fs           = require("fs");
 var config       = JSON.parse(fs.readFileSync("config.json"));
+var util         = require("util");
+var child        = require("child_process");
 
 exports.login = function(request, response){
 	routesCommon.login(1, request, response);
@@ -202,16 +204,58 @@ exports.mediafile = function(request,response){
 
 	if(request.method === 'POST'){
 		if(request.session && request.session.user){
+			console.log("\n\n\n *** FILES MOTHER FUCKER! *** \n" + util.inspect(request.files) + "\n\n\n");
 			var mediaFile = {
 				user: request.session.user.uuid,
 				title: request.body.title,
 				description: request.body.description,
 				course: request.body.course,
-				path: config.accentServer.mediaFolder + request.body.mediafile,
+				path: config.accentServer.tmpFolder + request.body.mediafile,
 				type: 0
 			}
 			MediaAction.addMediaFile(mediaFile, function(error, mediaFile){
 				if(!error){
+
+					//Convert media using ffmpeg
+					var args = [
+						'-i',
+						'/tmp/sample.avi',
+						'-vcodec', 'libx264',
+						'-flags', '+loop',
+						'-me_method', 'umh',
+						'-g', '250',
+						'-qcomp', '0.6',
+						'-qmin', '10',
+						'-qmax', '51',
+						'-qdiff', '4',
+						'-bf', '16',
+						'-b_strategy', '1', 
+						'-i_qfactor', '0.71', 
+						'-cmp', '+chroma',
+						'-subq', '8',
+						'-me_range', '16', 
+						'-coder', '1',
+						'-sc_threshold', '40', 
+						'-keyint_min', '25',
+						'-refs', '4',
+						'-trellis', '1',
+						'-partitions', '+parti8x8+parti4x4+partp8x8+partb8x8',
+						'-s', '720x480',
+						'-acodec', 'copy'/*libvo_aacenc*/,
+						'-ab', '128k',
+						'-threads', '0',
+						'-f', 'mp4', 
+						'media/derp'
+					]
+					var ffmpeg = child.spawn('ffmpeg', args);
+
+					ffmpeg.stderr.on('data', function (data) {
+					 	console.log('stderr: ' + data);
+					});
+
+					ffmpeg.on('exit', function(code){
+						console.log("DONE CONVERTING " + code);
+					})
 					var args = {
 						section: request.body.section,
 						material: mediaFile.uuid

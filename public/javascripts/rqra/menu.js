28acd63e83f20dc7f46c512f4cb5c995de42521e
jrf2 <jrf2@sfu.ca>
2012-07-26 17:22:04 -0700

    pushing the style sheet changes and images by me and Stanley.

--- public/javascripts/rqra/menu.js
+++ public/javascripts/rqra/menu.js
@@ -1,6 +1,25 @@
 var closeTimer = 0;
 var menuItem = 0;
 
+$(document).ready(function() {
+	$('#askQuestionInput').each(function() {
+		var default_value = this.value;
+		$(this).css('color', '#c6c6c6'); // this could be in the style sheet instead
+		$(this).focus(function() {
+			if(this.value == default_value) {
+				this.value = '';
+				$(this).css('color', '#4d4d4d');
+			}
+		});
+		$(this).blur(function() {
+			if(this.value == '') {
+				$(this).css('color', '#c6c6c6');
+				this.value = default_value;
+			}
+		});
+	});
+});
+
 // open menu
 function openMenu(id) {
 	if (menuItem) {

9bcbec5a334a1debd110528a1b04e71ae1116e55
jhc20 <jhc20@sfu.ca>
2012-07-30 13:23:05 -0700

    able to retrieve tag title and description

--- public/javascripts/accent/uploadTag.js
+++ public/javascripts/accent/uploadTag.js
@@ -1,8 +1,17 @@
 var common = new coreApi.Common();
 var rqra = new coreApi.Presenter();
 
-function uploadTag(){
+function uploadTag(tag){
+	var tagNode = $(tag).parent().parent().children("div");
+	//var tagTitle = $(tagNode).children("input#TagTitle");
+	//var tagDescription = $(tagNode).children("textarea#TagDescription");
+	var tagTitle = document.getElementById("TagTitle");
+	var tagDescription = document.getElementById("TagDescription");
+
 	console.log('upload tag now!');
+	console.log(tagNode);
+	console.log(tagTitle);
+	console.log(tagDescription);
 }
 
 

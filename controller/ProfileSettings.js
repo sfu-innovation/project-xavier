2213316764ae311c61ce8b1cd835f5561ef638fb
hrl1 <hrl1@sfu.ca>
2012-07-31 09:54:25 -0700

    Engage: fixed css and function in user

--- controller/ProfileSettings.js
+++ controller/ProfileSettings.js
@@ -14,86 +14,94 @@ var settings = exports.settings = function( req, callback ) {
 		msg="";
 	
 		if( req.method === 'POST') {
+
 			var filepath, path;
-
-			console.log(req.body)
-
-			/***
-			 *	Need to find proper way to limit upload size. 
-			 ***/
-		//	console.log(req.files.upload)
-			if(req.files.upload.size > 0) { //upload --> preview
-
-				if(req.files.upload.size < 5242880) {
-					var upload = req.files.upload.type.split('/'),
-						type = upload[0];
-					if (type === 'image'){
-						format = upload[1];
-						console.log('upload format: '+format)
-
-						path = req.files.upload.path;
-						filepath = './public/images/avatars/donotremove/'+req.session.user.uuid+'.'+format;
-
-						img = '/images/avatars/donotremove/'+req.session.user.uuid+'.'+format;
-					} else {
-						msg = "Error: Profile picture must be an image file."
-					}
-				} else {
-					msg = "Error: Profile picture must be less than 5MB in size."
-
-				
-				}
-			} else if (req.body.helper === 'del') { //delete --> preview
-				format = 'png';
-				console.log('delete format: '+format)
-
-				path = './public/images/engage/default_profile.png';
-				filepath = './public/images/avatars/donotremove/'+req.session.user.uuid+'.'+format;
-
-				img = '/images/engage/default_profile.png';
+	
+			if(req.files === undefined) { //probably notification settings
+				//notification settings
+				console.log(req.body)
+				console.log('no upload')
+			} //callback on end
+			else {
 			
-			} else { //saves
-				console.log('save format: '+format)
-				if(req.body.helper !== ''){ //has format, otherwise keep current img
-					var name = req.session.user.uuid+'.'+req.body.helper;
-					path = './public/images/avatars/donotremove/'+name;
-					filepath = './public/images/avatars/'+name;
 
-					img = '/images/avatars/'+name;
-				}	
+				/***
+				 *	Need to find proper way to limit upload size. 
+				 ***/
+			//	console.log(req.files.upload)
+				  if(req.files.upload.size > 0) { //upload --> preview
 
-				User.setPreferedName(req.session.user.uuid, req.body.pref_name,function(err, res){
-					if (err)
-						console.log(err)
-				});
-				UserProfile.updateProfile(req.session.user.uuid, {
-					profilePicture: img,
-					bio: req.body.bio
-				}, function(err, data) {	
-					if (err)
-						console.log(err)
-				})
+					if(req.files.upload.size < 5242880) {
+						var upload = req.files.upload.type.split('/'),
+							type = upload[0];
+						if (type === 'image'){
+							format = upload[1];
+							console.log('upload format: '+format)
 
-				req.session.user.preferedName = req.body.pref_name;
-				req.session.Profile.profilePicture = img;
-				req.session.Profile.bio = req.body.bio;
-				msg = 'Your changes have been saved.'
+							path = req.files.upload.path;
+							filepath = './public/images/avatars/donotremove/'+req.session.user.uuid+'.'+format;
+
+							img = '/images/avatars/donotremove/'+req.session.user.uuid+'.'+format;
+						} else {
+							msg = "Error: Profile picture must be an image file."
+						}
+					} else {
+						msg = "Error: Profile picture must be less than 5MB in size."
+
+					
+					}
+				} else if (req.body.helper === 'del') { //delete --> preview
+					format = 'png';
+					console.log('delete format: '+format)
+
+					path = './public/images/engage/default_profile.png';
+					filepath = './public/images/avatars/donotremove/'+req.session.user.uuid+'.'+format;
+
+					img = '/images/engage/default_profile.png';
 				
-			}
+				} else { //saves
+					console.log('save format: '+format)
+					if(req.body.helper !== ''){ //has format, otherwise keep current img
+						var name = req.session.user.uuid+'.'+req.body.helper;
+						path = './public/images/avatars/donotremove/'+name;
+						filepath = './public/images/avatars/'+name;
 
-			if(path){
-				fs.readFile(path, function (err, data) {
-				 	fs.writeFile(filepath, data, function (err) {
-				 		if(err)
-				 			console.log(err)
-				 	});
-				});
-			}
+						img = '/images/avatars/'+name;
+					}	
+
+					User.setPreferedName(req.session.user.uuid, req.body.pref_name,function(err, res){
+						if (err)
+							console.log(err)
+					});
+					UserProfile.updateProfile(req.session.user.uuid, {
+						profilePicture: img,
+						bio: req.body.bio
+					}, function(err, data) {	
+						if (err)
+							console.log(err)
+					})
+
+					req.session.user.preferedName = req.body.pref_name;
+					req.session.Profile.profilePicture = img;
+					req.session.Profile.bio = req.body.bio;
+					msg = 'Your changes have been saved.'
+					
+				}
+
+				if(path){
+					fs.readFile(path, function (err, data) {
+					 	fs.writeFile(filepath, data, function (err) {
+					 		if(err)
+					 			console.log(err)
+					 	});
+					});
+				}
 
 				pName = req.body.pref_name;
-				bio = req.body.bio;		
+				bio = req.body.bio;
+			}		
 
-		}
+		}//#if POST
 		profile.pName = pName;
 		profile.bio = bio;
 		profile.img = img;
